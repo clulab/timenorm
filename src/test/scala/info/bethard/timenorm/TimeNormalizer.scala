995a09e2eacf453df2bcaeed9960579572691a68
Steven Bethard <steven.bethard@gmail.com>
2013-03-25 17:33:39 -0600

    Adds another special case (for HHMMTZ times) to tokenization

--- src/test/scala/info/bethard/timenorm/TimeNormalizer.scala
+++ src/test/scala/info/bethard/timenorm/TimeNormalizer.scala
@@ -9,31 +9,39 @@ class TimeNormalizer(grammarURL: URL = classOf[TimeNormalizer].getResource("/tim
   private val grammar = SynchronousGrammar.fromString(grammarText)
   private val parser = new SynchronousParser(grammar)
 
-  private final val wordBoundary = "\\b|(?<=[^\\p{L}])(?=[\\p{L}])|(?<=[\\p{L}])(?=[^\\p{L}])".r
+  private final val wordBoundary = "\\b".r
+  private final val letterNonLetterBoundary = "(?<=[^\\p{L}])(?=[\\p{L}])|(?<=[\\p{L}])(?=[^\\p{L}])".r
 
   def parseAll(sourceText: String): Seq[TemporalParse] = {
-    val tokens = for (untrimmedToken <- this.wordBoundary.split(sourceText.toLowerCase())) yield {
-      val token = untrimmedToken.trim
-      if (token.isEmpty) {
+    val tokens = for (untrimmedWord <- this.wordBoundary.split(sourceText)) yield {
+      val word = untrimmedWord.trim
+      if (word.isEmpty) {
         Seq.empty[String]
-      } else if (token.matches("\\d{8}")) {
-        // special case for concatenated YYYYMMDD
-        Seq(token.substring(0, 4), "-", token.substring(4, 6), "-", token.substring(6, 8))
-      } else {
-        Seq(token)
+      }
+      // special case for concatenated YYYYMMDD
+      else if (word.matches("^\\d{8}$")) {
+        Seq(word.substring(0, 4), "-", word.substring(4, 6), "-", word.substring(6, 8))
+      }
+      // special case for concatenated HHMMTZ
+      else if (word.matches("^\\d{4}[A-Z]{3,4}$")) {
+        Seq(word.substring(0, 2), ":", word.substring(2, 4), word.substring(4).toLowerCase)
+      }
+      // otherwise, split at all letter/non-letter boundaries
+      else {
+        this.letterNonLetterBoundary.split(word).toSeq.map(_.trim.toLowerCase).filterNot(_.isEmpty)
       }
     }
     val parses = this.parser.parseAll(tokens.flatten)
     parses.toSeq.map(TemporalParse)
   }
-  
+
   def normalize(parse: TemporalParse, anchor: ZonedDateTime): Either[Period, TimeSpan] = {
     parse match {
       case parse: PeriodParse => Left(parse.toPeriod)
       case parse: TimeSpanParse => Right(parse.toTimeSpan(anchor))
     }
   }
-  
+
   def normalize(parses: Seq[TemporalParse], anchor: ZonedDateTime): Option[Either[Period, TimeSpan]] = {
     val temporals = for (parse <- parses) yield this.normalize(parse, anchor)
     temporals match {
