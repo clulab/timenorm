df4e78f5d706f8c00ea950cfca5500da6ac2710e
Steven Bethard <steven.bethard@gmail.com>
2013-05-06 16:13:18 +0200

    Fixes bugs in sample grammar's number parsing, adding tests to verify fixes.

---
+++ src/test/scala/info/bethard/timenorm/TemporalExpressionParserTest.scala
@@ -0,0 +1,42 @@
+package info.bethard.timenorm
+
+import scala.collection.immutable.Seq
+import scala.util.Success
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.threeten.bp.temporal.ChronoUnit._
+import org.threeten.bp.temporal.ChronoField._
+
+@RunWith(classOf[JUnitRunner])
+class TemporalExpressionParserTest extends FunSuite {
+
+  test("parses numbers") {
+    val parser = new TemporalExpressionParser
+    val anchor = TimeSpan.of(2013, 1, 3)
+    assert(parser.parse("two weeks", anchor) === Success(Period(Map(WEEKS -> 2))))
+    assert(parser.parse("thirteen months", anchor) === Success(Period(Map(MONTHS -> 13))))
+    assert(parser.parse("twenty four days", anchor) === Success(Period(Map(DAYS -> 24))))
+    assert(parser.parse("a hundred weeks", anchor) === Success(Period(Map(WEEKS -> 100))))
+    assert(parser.parse("a hundred six minutes", anchor) === Success(Period(Map(MINUTES -> 106))))
+    assert(parser.parse("a hundred twelve seconds", anchor) === Success(Period(Map(SECONDS -> 112))))
+    assert(parser.parse("a hundred fifty-five years", anchor) === Success(Period(Map(YEARS -> 155))))
+    assert(parser.parse("eight hundred nine decades", anchor) === Success(Period(Map(DECADES -> 809))))
+    assert(parser.parse("two hundred seventeen hours", anchor) === Success(Period(Map(HOURS -> 217))))
+    assert(parser.parse("six hundred eighty seven months", anchor) === Success(Period(Map(MONTHS -> 687))))
+    assert(parser.parse("a thousand days", anchor) === Success(Period(Map(DAYS -> 1000))))
+    assert(parser.parse("a thousand one days", anchor) === Success(Period(Map(DAYS -> 1001))))
+    assert(parser.parse("a thousand ten days", anchor) === Success(Period(Map(DAYS -> 1010))))
+    assert(parser.parse("a thousand twenty days", anchor) === Success(Period(Map(DAYS -> 1020))))
+    assert(parser.parse("a thousand forty-two days", anchor) === Success(Period(Map(DAYS -> 1042))))
+    assert(parser.parse("a thousand one hundred days", anchor) === Success(Period(Map(DAYS -> 1100))))
+    assert(parser.parse("a thousand one hundred ten days", anchor) === Success(Period(Map(DAYS -> 1110))))
+    assert(parser.parse("one thousand one days", anchor) === Success(Period(Map(DAYS -> 1001))))
+    assert(parser.parse("two thousand", anchor) === Success(TimeSpan.fromTimeMLValue("2000")))
+    assert(parser.parse("two thousand one", anchor) === Success(TimeSpan.fromTimeMLValue("2001")))
+    assert(parser.parse("two thousand fifteen", anchor) === Success(TimeSpan.fromTimeMLValue("2015")))
+    assert(parser.parse("two thousand two hundred", anchor) === Success(TimeSpan.fromTimeMLValue("2200")))
+    assert(parser.parse("two thousand two hundred two", anchor) === Success(TimeSpan.fromTimeMLValue("2202")))
+    assert(parser.parse("twenty twenty-three", anchor) === Success(TimeSpan.fromTimeMLValue("2023")))
+  }
+}
