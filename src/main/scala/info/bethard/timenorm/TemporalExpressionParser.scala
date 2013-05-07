f00d0d256c4fe72ed48281bb59188b4451060cad
Steven Bethard <steven.bethard@gmail.com>
2013-05-07 16:59:01 +0200

    Documents TemporalExpressionParser

--- src/main/scala/info/bethard/timenorm/TemporalExpressionParser.scala
+++ src/main/scala/info/bethard/timenorm/TemporalExpressionParser.scala
@@ -18,7 +18,25 @@ import info.bethard.timenorm.parse.PeriodSetParse
 import info.bethard.timenorm.parse.TimeSpanParse
 import info.bethard.timenorm.parse.TimeSpanSetParse
 
-
+/**
+ * A parser for natural language expressions of time, based on a synchronous context free grammar.
+ * Typical usage:
+ * {{{
+    // create a new parser (using the default English grammar)
+    val parser = new TemporalExpressionParser
+    // establish an anchor time
+    val anchor = TimeSpan.of(2013, 1, 4)
+    // parse an expression given an anchor time (assuming here that it succeeeds)
+    val Success(temporal) = parser.parse("two weeks ago", anchor)
+    // get the TimeML value ("2012-W51") from the Temporal
+    val value = temporal.timeMLValue
+ * }}}
+ * 
+ * @constructor Creates a parser from a URL to a grammar file.
+ * @param grammarURL The URL of a grammar file, in [[SynchronousGrammar.fromString]] format. If not
+ *        specified, the default English grammar on the classpath is used. Note that if another
+ *        grammar is specified, it may be necessary to override the [[tokenize]] method.
+ */
 class TemporalExpressionParser(grammarURL: URL = classOf[TemporalExpressionParser].getResource("/info/bethard/timenorm/en.grammar")) {
   private val logger = Logger.getLogger(this.getClass.getName)
   private val grammarText = Source.fromURL(grammarURL, "US-ASCII").mkString
@@ -29,7 +47,16 @@ class TemporalExpressionParser(grammarURL: URL = classOf[TemporalExpressionParse
   private final val wordBoundary = "\\b".r
   private final val letterNonLetterBoundary = "(?<=[^\\p{L}])(?=[\\p{L}])|(?<=[\\p{L}])(?=[^\\p{L}])".r
 
-  def tokenize(sourceText: String): IndexedSeq[String] = {
+  /**
+   * Splits a string into tokens to be used as input for the synchronous parser.
+   * 
+   * This method may be overridden by subclasses if a grammar requires different tokenization than
+   * the default English grammar on the classpath.
+   * 
+   * @param sourceText The input text.
+   * @return The tokens that result from splitting the input text.
+   */
+  protected def tokenize(sourceText: String): IndexedSeq[String] = {
     val tokens = for (untrimmedWord <- this.wordBoundary.split(sourceText)) yield {
       val word = untrimmedWord.trim
       if (word.isEmpty) {
@@ -59,10 +86,25 @@ class TemporalExpressionParser(grammarURL: URL = classOf[TemporalExpressionParse
     filteredTokens.toIndexedSeq
   }
 
+  /**
+   * Tries to parse a source string into a single [[Temporal]] object. 
+   * 
+   * @param sourceText The input string in the source language.
+   * @param anchor The anchor time (required for resolving relative times like "today").
+   * @return The most likely [[Temporal]] parse according to the parser's heuristic.
+   */
   def parse(sourceText: String, anchor: TimeSpan): Try[Temporal] = {
     this.parseAll(sourceText, anchor).map(_.head)
   }
 
+  /**
+   * Try to parse a source string into possible [[Temporal]] objects.
+   * 
+   * @param sourceText The input string in the source language.
+   * @param anchor The anchor time (required for resolving relative times like "today").
+   * @return A sequence of [[Temporal]] objects representing the possible parses. The sequence is
+   *         sorted by a heuristic that tries to put the most promising parses first.
+   */
   def parseAll(sourceText: String, anchor: TimeSpan): Try[Seq[Temporal]] = {
     // tokenize the string
     val tokens = this.tokenize(sourceText)
