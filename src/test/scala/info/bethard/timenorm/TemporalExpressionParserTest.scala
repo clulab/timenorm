98141509efd3203c409b8a20dc6f897acf3156d1
Steven Bethard <steven.bethard@gmail.com>
2013-05-07 13:30:06 +0200

    Documents info.bethard.timenorm package

--- src/test/scala/info/bethard/timenorm/TemporalExpressionParserTest.scala
+++ src/test/scala/info/bethard/timenorm/TemporalExpressionParserTest.scala
@@ -11,6 +11,18 @@ import org.threeten.bp.temporal.ChronoField._
 @RunWith(classOf[JUnitRunner])
 class TemporalExpressionParserTest extends FunSuite {
 
+  test("works as described in documentation") {
+    // create a new parser (using the default English grammar)
+    val parser = new TemporalExpressionParser
+    // establish an anchor time
+    val anchor = TimeSpan.of(2013, 1, 4)
+    // parse an expression given an anchor time (assuming here that it succeeeds)
+    val Success(temporal) = parser.parse("two weeks ago", anchor)
+    // get the TimeML value ("2012-W51") from the Temporal
+    val value = temporal.timeMLValue
+    assert(value === "2012-W51")
+  }
+
   test("parses numbers") {
     val parser = new TemporalExpressionParser
     val anchor = TimeSpan.of(2013, 1, 3)
