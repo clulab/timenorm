5a4aa42a24c49fcdc7ba6cf54f9ffb65fc713afd
Steven Bethard <steven.bethard@gmail.com>
2013-07-29 15:11:33 -0600

    Fixes typo in package documentation.

--- src/main/scala/info/bethard/timenorm/package.scala
+++ src/main/scala/info/bethard/timenorm/package.scala
@@ -10,7 +10,7 @@ package info.bethard
     val parser = new TemporalExpressionParser
     // establish an anchor time
     val anchor = TimeSpan.of(2013, 1, 4)
-    // parse an expression given an anchor time (assuming here that it succeeeds)
+    // parse an expression given an anchor time (assuming here that it succeeds)
     val Success(temporal) = parser.parse("two weeks ago", anchor)
     // get the TimeML value ("2012-W51") from the Temporal
     val value = temporal.timeMLValue
