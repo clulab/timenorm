23c9c32bc3d2a77a4275639cf663297c491127d0
Steven Bethard <steven.bethard@gmail.com>
2015-12-22 14:10:05 -0600

    Removes some unnecessary braces

--- src/main/scala/info/bethard/timenorm/formal/Readers.scala
+++ src/main/scala/info/bethard/timenorm/formal/Readers.scala
@@ -18,11 +18,10 @@ object AnaforaReader {
 
   def period(entity: Entity)(implicit data: Data): Period = entity.`type` match {
     case "Period" => entity.properties("Type") match {
-      case "Unknown" => {
+      case "Unknown" =>
         assert(!entity.properties.has("Number"), s"expected empty Number, found ${entity.xml}")
         assert(!entity.properties.has("Modifier"), s"expected empty Modifier, found ${entity.xml}")
         UnknownPeriod
-      }
       case _ => SimplePeriod(
         ChronoUnit.valueOf(entity.properties("Type").toUpperCase()),
         number(entity.properties.entity("Number")),
@@ -46,33 +45,29 @@ object AnaforaReader {
                        periodFunc: (Interval, Period) => Interval,
                        repeatingIntervalFunc: (Interval, RepeatingInterval) => Interval)(implicit data: Data): Interval = {
     entity.properties.getEntity("Period") match {
-      case Some(periodEntity) => {
+      case Some(periodEntity) =>
         assert(!entity.properties.has("Repeating-Interval"),
           s"expected empty Repeating-Interval, found ${entity.xml}")
         periodFunc(interval(entity.properties), period(periodEntity))
-      }
-      case None => {
+      case None =>
         assert(entity.properties.has("Repeating-Interval"),
           s"expected Repeating-Interval, found ${entity.xml}")
         assert(!entity.properties.has("Repeating-Interval-Number"),
           s"expected empty Repeating-Interval-Number, found ${entity.xml}")
         val repeatingIntervalEntity = entity.properties.entity("Repeating-Interval")
         repeatingIntervalFunc(interval(entity.properties), repeatingInterval(repeatingIntervalEntity))
-      }
     }
   }
 
   def repeatingInterval(entity: Entity)(implicit data: Data): RepeatingInterval = entity.`type` match {
-    case "Calendar-Interval" => {
+    case "Calendar-Interval" =>
       UnitRepeatingInterval(ChronoUnit.valueOf(entity.properties("Type").toUpperCase + "S"))
-    }
-    case name => {
+    case name =>
       val field = ChronoField.valueOf(name.replace('-', '_').toUpperCase())
       val value = field match {
         case ChronoField.MONTH_OF_YEAR => Month.valueOf(entity.properties("Type").toUpperCase()).getValue
       }
       FieldRepeatingInterval(field, value)
-    }
   }
 
   def temporal(entity: Entity)(implicit data: Data): Temporal = entity.`type` match {
