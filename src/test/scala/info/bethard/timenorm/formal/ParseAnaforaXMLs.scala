7f94f5a7e14d3a907293a1b70ca3b93bf14df2c5
Steven Bethard <steven.bethard@gmail.com>
2015-12-22 15:20:08 -0600

    Print out path while printing parsed data

--- src/test/scala/info/bethard/timenorm/formal/ParseAnaforaXMLs.scala
+++ src/test/scala/info/bethard/timenorm/formal/ParseAnaforaXMLs.scala
@@ -5,6 +5,7 @@ import info.bethard.anafora.Data
 object ParseAnaforaXMLs {
   def main(args: Array[String]): Unit = {
     for (path <- args) {
+      println(path)
       implicit val data = Data.fromPath(path)
       for (entity <- data.entities) {
         println(AnaforaReader.temporal(entity))
