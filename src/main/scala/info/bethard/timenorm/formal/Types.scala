d63f1e6cb9d0d553ed5fc1c2b3572a4b078125a2
Andrew M <awmarkley@users.noreply.github.com>
2016-02-04 12:38:48 -0600

    Added Year, Decade, Century, and TwoDigitYear. Added documentation for Interval Trait.

--- src/main/scala/info/bethard/timenorm/formal/Types.scala
+++ src/main/scala/info/bethard/timenorm/formal/Types.scala
@@ -1,6 +1,7 @@
 package info.bethard.timenorm.formal
 
 import java.time.temporal.{TemporalField, TemporalUnit}
+import java.time.LocalDateTime
 
 trait Temporal
 
@@ -26,27 +27,128 @@ case class SimplePeriod(unit: TemporalUnit, n: Number, modifier: Modifier) exten
 case object UnknownPeriod extends Period
 case class PeriodSum(periods: Set[Period], modifier: Modifier) extends Period
 
-trait Interval extends Temporal
-case object DocumentCreationTime extends Interval
-case object UnknownInterval extends Interval
-case class Event(description: String) extends Interval
-case class Year(n: Int) extends Interval
-case class Decade(n: Int) extends Interval
-case class Century(n: Int) extends Interval
-case class TwoDigitYear(interval: Interval, twoDigits: Int) extends Interval
-case class ThisPeriod(interval: Interval, period: Period) extends Interval
-case class ThisRepeatingInterval(interval: Interval, repeatingInterval: RepeatingInterval) extends Interval
-case class LastPeriod(interval: Interval, period: Period) extends Interval
-case class LastRepeatingInterval(interval: Interval, repeatingInterval: RepeatingInterval) extends Interval
-case class NextPeriod(interval: Interval, period: Period) extends Interval
-case class NextRepeatingInterval(interval: Interval, repeatingInterval: RepeatingInterval) extends Interval
-case class BeforePeriod(interval: Interval, period: Period) extends Interval
-case class BeforeRepeatingInterval(interval: Interval, repeatingInterval: RepeatingInterval) extends Interval
-case class AfterPeriod(interval: Interval, period: Period) extends Interval
-case class AfterRepeatingInterval(interval: Interval, repeatingInterval: RepeatingInterval) extends Interval
-case class Between(startInterval: Interval, endInterval: Interval) extends Interval
-case class Nth(interval: Interval, value: Int, repeatingInterval: RepeatingInterval) extends Interval
-case class IntervalSubIntervalIntersection(interval: Interval, subInterval: RepeatingInterval) extends Interval
+/**
+  * An interval on the timeline, defined by a starting point using the start val (inclusive) and an ending
+  * point expressed by the end val (exclusive). For example, the expression \textit{1990} corresponds to the
+  * interval [1990-01-01, 1991-01-01).
+  */
+trait Interval extends Temporal {
+  val start : LocalDateTime
+  val end : LocalDateTime
+}
+
+case object DocumentCreationTime extends Interval {
+  val start = ???
+  val end = ???
+}
+
+case object UnknownInterval extends Interval {
+  val start = ???
+  val end = ???
+}
+
+case class Event(description: String) extends Interval {
+  val start = ???
+  val end = ???
+}
+
+/**
+ * A Year represents the interval from the first second of the year (inclusive) to the first second of the
+ * next year (exclusive).
+ */
+case class Year(n: Int) extends Interval {
+  val start = LocalDateTime.of( n, 1, 1, 0, 0, 0, 0 )
+  val end = start.plusYears( 1 )
+}
+
+/**
+ * A Decade represents the interval from the first second of the decade (inclusive) to the first second of the
+ * next decade (exclusive).
+ */
+case class Decade(n: Int) extends Interval {
+  val start = LocalDateTime.of( n * 10, 1, 1, 0, 0, 0, 0 )
+  val end = start.plusYears( 10 )
+}
+
+/**
+ * A Century represents an interval from the first second of the century (inclusive) to the first second
+ * of the next century (exclusive).
+ */
+case class Century(n: Int) extends Interval {
+  val start = LocalDateTime.of( n * 100, 1, 1, 0, 0, 0, 0 )
+  val end = start.plusYears( 100 )
+}
+
+/**
+ * TwoDigitYear creates a one year interval from two digits and the century of another interval.
+ * Formally: TwoDigitYear([ABCD-EF-GH,...) : Interval, YZ : Integer) = [ABYZ-01-01, (ABYZ+1)-01-01)
+ */
+case class TwoDigitYear(interval: Interval, twoDigits: Int) extends Interval {
+  val start = LocalDateTime.of( ( interval.start.getYear() / 100 * 100 ) + twoDigits, 
+      1, 1, 0, 0, 0, 0 )
+  val end = start.plusYears( 1 )
+}
+
+case class ThisPeriod(interval: Interval, period: Period) extends Interval {
+  val start = ???
+  val end = ???
+}
+case class ThisRepeatingInterval(interval: Interval, repeatingInterval: RepeatingInterval) extends Interval {
+  val start = ???
+  val end = ???
+}
+case class LastPeriod(interval: Interval, period: Period) extends Interval {
+  val start = ???
+  val end = ???
+}
+case class LastRepeatingInterval(interval: Interval, repeatingInterval: RepeatingInterval) extends Interval {
+  val start = ???
+  val end = ???
+}
+case class NextPeriod(interval: Interval, period: Period) extends Interval {
+  val start = ???
+  val end = ???
+}
+
+case class NextRepeatingInterval(interval: Interval, repeatingInterval: RepeatingInterval) extends Interval {
+  val start = ???
+  val end = ???
+}
+
+case class BeforePeriod(interval: Interval, period: Period) extends Interval {
+  val start = ???
+  val end = ???
+}
+
+case class BeforeRepeatingInterval(interval: Interval, repeatingInterval: RepeatingInterval) extends Interval {
+  val start = ???
+  val end = ???
+}
+
+case class AfterPeriod(interval: Interval, period: Period) extends Interval {
+  val start = ???
+  val end = ???
+}
+
+case class AfterRepeatingInterval(interval: Interval, repeatingInterval: RepeatingInterval) extends Interval {
+  val start = ???
+  val end = ???
+}
+
+case class Between(startInterval: Interval, endInterval: Interval) extends Interval {
+  val start = ???
+  val end = ???
+}
+
+case class Nth(interval: Interval, value: Int, repeatingInterval: RepeatingInterval) extends Interval {
+  val start = ???
+  val end = ???
+}
+
+case class IntervalSubIntervalIntersection(interval: Interval, subInterval: RepeatingInterval) extends Interval {
+  val start = ???
+  val end = ???
+}
 
 trait RepeatingInterval extends Temporal
 case class UnitRepeatingInterval(unit: TemporalUnit, modifier: Modifier) extends RepeatingInterval
---
+++ src/test/scala/info/bethard/timenorm/formal/TypesTest.scala
@@ -0,0 +1,48 @@
+package info.bethard.timenorm.formal
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import java.time.LocalDateTime
+
+@RunWith(classOf[JUnitRunner])
+class TypesTest extends FunSuite {
+  
+  test( "Year Type" ) {
+    val year = Year( 1985 )
+    
+    assert( year.start === LocalDateTime.of( 1985, 1, 1, 0, 0, 0, 0 ))
+    assert( year.end === LocalDateTime.of( 1986, 1, 1, 0, 0, 0, 0 ) )
+  }
+  
+  test( "Decade Type" ) {
+    val decade = Decade( 198 )
+    
+    assert( decade.start === LocalDateTime.of( 1980, 1, 1, 0, 0, 0, 0 ))
+    assert( decade.end === LocalDateTime.of( 1990, 1, 1, 0, 0, 0, 0 )) 
+  }
+  
+  test( "Century Type" ) {
+    val century = Century( 17 )
+    
+    assert( century.start === LocalDateTime.of( 1700, 1, 1, 0, 0, 0, 0 ))
+    assert( century.end === LocalDateTime.of( 1800, 1, 1, 0, 0, 0, 0 )) 
+  }
+  
+  test( "TwoDigitYear Type" ) {
+    val fromYear = TwoDigitYear( Year( 1903 ), 37 )
+    assert( fromYear.start === LocalDateTime.of( 1937, 1, 1, 0, 0, 0, 0 ))
+    assert( fromYear.end === LocalDateTime.of( 1938, 1, 1, 0, 0, 0, 0 )) 
+    
+    val fromDecade = TwoDigitYear( Decade( 132 ), 85 )
+    assert( fromDecade.start === LocalDateTime.of( 1385, 1, 1, 0, 0, 0, 0 ))
+    assert( fromDecade.end === LocalDateTime.of( 1386, 1, 1, 0, 0, 0, 0 )) 
+    
+    val fromCentury = TwoDigitYear( Century( 23 ), 22 )
+    assert( fromCentury.start === LocalDateTime.of( 2322, 1, 1, 0, 0, 0, 0 ))
+    assert( fromCentury.end === LocalDateTime.of( 2323, 1, 1, 0, 0, 0, 0 )) 
+    
+  }
+  
+}
+
