31819df0b49bf9a05758a93985cd5b455b822d8c
EgoLaparra <egoitz.laparra@ehu.eus>
2018-04-03 16:35:26 -0700

    read schema

--- src/main/scala/org/clulab/timenorm/TemporalCharbasedParser.scala
+++ src/main/scala/org/clulab/timenorm/TemporalCharbasedParser.scala
@@ -10,17 +10,18 @@ import org.deeplearning4j.nn.conf.ComputationGraphConfiguration
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.deeplearning4j.nn.conf.inputs.InputType
 
-import java.util.Arrays
 import scala.collection.immutable.IndexedSeq
 import scala.io.Source
 import scala.util.Failure
 import scala.util.Success
 import scala.util.Try
+import scala.xml.XML
 
+import java.util.Arrays
+import java.io.{InputStream, FileInputStream}
 import java.time.DateTimeException
 import java.time.LocalDate
 import java.time.temporal.IsoFields.QUARTER_YEARS
-import java.io.{InputStream, FileInputStream}
 
 import play.api.libs.json._
 
@@ -28,14 +29,6 @@ object TemporalCharbasedParser {
   private val log: Logger = LoggerFactory.getLogger(TemporalCharbasedParser.getClass)
 
   def main(args: Array[String]): Unit = {
-
-    println(System.getProperty("java.library.path"))
-    System.setProperty("java.library.path",
-			 System.getProperty("java.library.path") +
-			   java.io.File.pathSeparator +
-                           "/home/egoitz/Tools/deeplearning/nd4j/nd4j-backends/nd4j-backend-impls/nd4j-native/target/classes/")
-    println(System.getProperty("java.library.path"))
-
     val parser = args match {
       case Array(modelFile) =>
         new TemporalCharbasedParser(modelFile)
@@ -73,8 +66,13 @@ class TemporalCharbasedParser(modelFile: String) {
   private val unicode2int = readDict(this.getClass.getResourceAsStream("/org/clulab/timenorm/vocab/unicate2int.txt"))
   private val operatorLabels = Source.fromInputStream(this.getClass.getResourceAsStream("/org/clulab/timenorm/label/operator.txt")).getLines.toList
   private val nonOperatorLabels = Source.fromInputStream(this.getClass.getResourceAsStream("/org/clulab/timenorm/label/non-operator.txt")).getLines.toList
+  private val schema = (for (es <- XML.loadFile(this.getClass.getResource("/org/clulab/timenorm/linking_configure/timenorm-schema.xml").getPath) \\ "entities") yield for (e <- es \ "entity") yield (for (p <- e \\ "property" ) yield ((e \ "@type" head).toString, ((p \ "@type" head).toString, (Try((p \ "@required" head).toString.toBoolean).getOrElse(true), Try((p \ "@instanceOf" head).toString.split(",")).getOrElse(Array()))))) :+  ((e \ "@type" head).toString, ("parentType", (true, Array((es \ "@type" head).toString))))).flatten.flatten.groupBy(_._1).mapValues(_.map(_._2).toMap)
 
   private val unicodes = Array("Cn", "Lu", "Ll", "Lt", "Lm", "Lo", "Mn", "Me", "Mc", "Nd", "Nl", "No", "Zs", "Zl", "Zp", "Cc", "Cf", "Cn", "Co", "Cs", "Pd", "Ps", "Pe", "Pc", "Po", "Sm", "Sc", "Sk", "So", "Pi", "Pf")
+
+  private def printModel(){
+    println(this.network.summary())
+  }
 
 
 // UNASSIGNED = 0 [Cn]	Other, Not Assigned (no characters in the file have this property)
@@ -115,7 +113,13 @@ class TemporalCharbasedParser(modelFile: String) {
 
 
   def parse(sourceText: String, anchor: TimeSpan){ //: Try[Temporal] = {
+    val entities = identification(sourceText)
+    println(entities.toString)
+    linking(entities)
+  }
 
+
+  def identification(sourceText: String): List[(Int, Int, String)] = {
     val input0 = Nd4j.create(sourceText.map(c => this.char2int.getOrElse(c.toString(), this.char2int("unknown"))).toArray, Array(1,1,sourceText.length))
     val input1 = Nd4j.create(sourceText.map(c => this.unicode2int.getOrElse(unicodes(c.getType), this.unicode2int("unknown"))).toArray, Array(1,1,sourceText.length))
 
@@ -123,22 +127,25 @@ class TemporalCharbasedParser(modelFile: String) {
     this.network.setInput(1, input1)
     val results = this.network.feedForward()
 
-    //this.printModel()
-    //print(results.get(sourceText))
-    //println(results.get(sourceText).shape().toList)
-    //for(r <- results.get(sourceText).toFloatMatrix()) println(r.toList)
-    //println(results.get("timedistributed_1"))
-    //println(results.get("timedistributed_1").getColumn(0).max())
+    val labels = (x: Array[Array[Float]]) => (for (r <- x) yield r.indexWhere(i => (i == r.max))).toList.map(o => Try(nonOperatorLabels(o-1)).getOrElse("O")).drop(3).dropRight(3)
+    val spans = (x: List[String]) => ("" +: x :+ "").sliding(2).zipWithIndex.filter(f => f._1(0) != f._1(1)).sliding(2).map(m =>(m(0)._2, m(1)._2-1, m(0)._1(1))).filter(_._3 != "O").toList
 
-    val nonOperators = (for (r <- results.get("timedistributed_1").toFloatMatrix()) yield r.indexWhere(x => (x == r.max))).toList.map(o => Try(nonOperatorLabels(o-1)).getOrElse("O"))
-    println(nonOperators.toString)
-    val expOperators = (for (r <- results.get("timedistributed_2").toFloatMatrix()) yield r.indexWhere(x => (x == r.max))).toList.map(o => Try(operatorLabels(o-1)).getOrElse("O"))
-    println(expOperators.toString)
-    val impOperators = (for (r <- results.get("timedistributed_3").toFloatMatrix()) yield r.indexWhere(x => (x == r.max))).toList.map(o => Try(operatorLabels(o-1)).getOrElse("O"))
-    println(impOperators.toString)
+    val nonOperators = labels(results.get("timedistributed_1").toFloatMatrix())
+    val expOperators = labels(results.get("timedistributed_2").toFloatMatrix())
+    val impOperators = labels(results.get("timedistributed_3").toFloatMatrix())
+
+    (spans(nonOperators) ::: spans(expOperators) ::: spans(impOperators)).sorted
   }
 
-  def printModel(){
-    println(this.network.summary())
+  def linking(entities: List[(Int, Int, String)]) = {
+    var stack = Array(0, 0)
+    for (entity <- entities) {
+      if (stack(0) != stack(1) && entity._1 - entities(stack(1)-1)._2 > 10)
+        stack(0) = stack(1)
+      for (s <- (stack(0) to stack(1) - 1).toList.reverse) {
+        println(entity, entities(s))
+      }
+      stack(1) += 1
+    }
   }
 }
