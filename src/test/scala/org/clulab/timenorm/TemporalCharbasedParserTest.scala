a2d938c07cf63d1fddb428f6ba623b441e15661a
EgoLaparra <egoitz.laparra@ehu.eus>
2018-05-30 14:47:09 -0700

    test changed

--- src/test/scala/org/clulab/timenorm/TemporalCharbasedParserTest.scala
+++ src/test/scala/org/clulab/timenorm/TemporalCharbasedParserTest.scala
@@ -5,38 +5,41 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSuite
 import java.time.temporal.ChronoUnit._
-import java.time.LocalDate
+import java.time.LocalDateTime
 
 @RunWith(classOf[JUnitRunner])
 class TemporalCharbasedParserTest extends FunSuite {
 
   val modelPath = this.getClass.getResource("/org/clulab/timenorm/model/char-3softmax-extra/lstm_models_2features.hdf5").getPath()
   val parser = new TemporalCharbasedParser(modelPath)
-  val now = LocalDate.now()
+  val now = LocalDateTime.now()
   val anchor = TimeSpan.of(now.getYear, now.getMonthValue, now.getDayOfMonth)
 
   test("parse-interval") {
     val date = "2018-10-10"
     val data = parser.parse("\n\n\n" + date + "\n\n\n", anchor)
-    for (ti <- parser.intervals(data); i <- ti._2) {
-      println(s"${ti._1._1}, ${ti._1._2} ${i._1} ${i._2}")
-    }
+
+    assert(parser.intervals(data)(0)._1 == (0, 10))
+    assert(parser.intervals(data)(0)._2(0)._1 === LocalDateTime.of(2018, 10, 10, 0, 0))
+    assert(parser.intervals(data)(0)._2(0)._2 === 86400)
   }
 
   test("parse-repeatingInterval") {
     val date = "January"
     val data = parser.parse("\n\n\n" + date + "\n\n\n", anchor)
-    for (ti <- parser.intervals(data); i <- ti._2) {
-      println(s"${ti._1._1}, ${ti._1._2} ${i._1} ${i._2}")
-    }
+
+    assert(parser.intervals(data)(0)._1 == (0, 7))
+    assert(parser.intervals(data)(0)._2(0)._1 === null)
+    assert(parser.intervals(data)(0)._2(0)._2 === 2678400)
   }
 
-  test("parse-error") {
+  test("parse-undef") {
     val date = "Friday."
     val data = parser.parse("\n\n\n" + date + "\n\n\n", anchor)
-    for (ti <- parser.intervals(data); i <- ti._2) {
-      println(s"${ti._1._1}, ${ti._1._2} ${i._1} ${i._2}")
-    }
+
+    assert(parser.intervals(data)(0)._1 == (0, 6))
+    assert(parser.intervals(data)(0)._2(0)._1 === null)
+    assert(parser.intervals(data)(0)._2(0)._2 === 0)
   }
 
 }
